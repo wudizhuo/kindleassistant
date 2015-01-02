fb0e416b9a755ee7372a300ed1ae65291bf587b1
SunZhuo_Work <SunZhuo@SunZhuo-PC>
2015-01-02 12:09:51 +0800

    网络

--- KindleAssistant/src/com/kindleassistant/activity/MainActivity.java
+++ KindleAssistant/src/com/kindleassistant/activity/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	
-		VolleyMgr.getInstance().getJson(AppConstants.SEND_URL);
 	}
 
 }
