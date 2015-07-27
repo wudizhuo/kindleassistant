a191b3a86502da93456877a190f405d099fa6f07
sun zhuo <sunzhuo1228@126.com>
2015-07-27 23:30:07 +0800

    update

--- app/src/main/java/com/kindleassistant/entity/ProgressListener.java
+++
@@ -1,8 +0,0 @@
-package com.kindleassistant.entity;
-
-/**
- * 进度监听器接口
- */
-public interface ProgressListener {
-	public void transferred(long transferedBytes);
-}
--- app/src/main/java/com/kindleassistant/entity/SendUrlRsp.java
+++
@@ -1,7 +0,0 @@
-package com.kindleassistant.entity;
-
-import com.kindleassistant.common.BaseResponse;
-
-public class SendUrlRsp extends BaseResponse {
-
-}
