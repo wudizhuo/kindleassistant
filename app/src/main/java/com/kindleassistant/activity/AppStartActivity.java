014340d23701ddc271d7601f7a394679795cf9dc
sun zhuo <sunzhuo1228@126.com>
2016-06-06 23:43:59 +0800

    remove unused code

--- app/src/main/java/com/kindleassistant/activity/AppStartActivity.java
+++ app/src/main/java/com/kindleassistant/activity/AppStartActivity.java
@@ -3,7 +3,6 @@ package com.kindleassistant.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.kindleassistant.R;
 import com.kindleassistant.common.BaseActivity;
 import com.kindleassistant.utils.ChannelUtil;
 import com.umeng.analytics.AnalyticsConfig;
@@ -13,7 +12,6 @@ public class AppStartActivity extends BaseActivity{
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);	
-		setContentView(R.layout.appstart);
 		AnalyticsConfig.setChannel(ChannelUtil.getUmengChannal(this));
 		Intent intent = new Intent (AppStartActivity.this,MainActivity.class);			
 		startActivity(intent);			
