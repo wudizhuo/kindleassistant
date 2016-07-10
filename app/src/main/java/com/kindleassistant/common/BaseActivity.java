02507f30868222062a404a25896bd59d3314d33e
sun zhuo <sunzhuo1228@126.com>
2016-07-10 18:40:25 +0800

    add system progress bar

--- app/src/main/java/com/kindleassistant/common/BaseActivity.java
+++ app/src/main/java/com/kindleassistant/common/BaseActivity.java
@@ -1,102 +1,103 @@
 package com.kindleassistant.common;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.kindleassistant.R;
-import com.kindleassistant.view.CustomerProgressDialog;
 import com.umeng.analytics.MobclickAgent;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    protected ProgressDialog loadingDialog;
+    protected boolean isLoading = false;
 
-	private CustomerProgressDialog progressDialog;
-	private boolean progressOnShow = false;
-	private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		MobclickAgent.onResume(this);
-	}
+    @Override
+    public void setSupportActionBar(Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
+    }
 
-	@Override
-	public void setSupportActionBar(Toolbar toolbar) {
-		super.setSupportActionBar(toolbar);
-		toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
-	}
+    Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            return false;
+        }
+    };
 
-	Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
-		@Override
-		public boolean onMenuItemClick(MenuItem menuItem) {
-			return false;
-		}
-	};
+    @Override
+    protected void onPause() {
+        MobclickAgent.onPause(this);
+        super.onPause();
+    }
 
-	@Override
-	protected void onPause() {
-		MobclickAgent.onPause(this);
-		super.onPause();
-	}
+    public void showProgressDialog(String message) {
+        dismissProgressDialog();
 
-	/**
-	 * 显示等待对话框
-	 * 
-	 * @param msg
-	 *            对话框显示的字体
-	 */
-	public void showProgressDialog(String msg) {
-		if (!isFinishing() && !progressOnShow) {
-			progressDialog = new CustomerProgressDialog(this);
-			if (!TextUtils.isEmpty(msg)) {
-				progressDialog.setMessage(msg);
-			}
-			progressDialog.show();
-			progressOnShow = true;
-		}
-	}
+        loadingDialog = new ProgressDialog(this);
+        loadingDialog.setMessage(message);
+        loadingDialog.setIndeterminate(false);
+        loadingDialog.setCanceledOnTouchOutside(false);
 
-	@Override
-	public void setContentView(int layoutResID) {
-		super.setContentView(layoutResID);
-		initTitleBar();
-	}
+        if (!isFinishing()) {
+            loadingDialog.show();
+        }
 
-	private void initTitleBar() {
-		if (getSupportActionBar() == null && findViewById(R.id.view_title) != null) {
-			toolbar = (Toolbar) findViewById(R.id.view_title);
-			setSupportActionBar(toolbar);
-		}
-	}
-	
-	/**
-	 * 显示等待对话框
-	 */
-	public void showProgressDialog() {
-		this.showProgressDialog(null);
-	}
+        isLoading = true;
+    }
 
-	/**
-	 * 取消等待对话框
-	 */
-	public void dismissProgressDialog() {
-		if (!isFinishing() && progressDialog != null
-				&& progressDialog.isShowing()) {
-			progressDialog.dismiss();
-			progressOnShow = false;
-		}
-	}
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        initTitleBar();
+    }
 
-	public void onBackPressed(View v) {
-		onBackPressed();
-	}
+    private void initTitleBar() {
+        if (getSupportActionBar() == null && findViewById(R.id.view_title) != null) {
+            toolbar = (Toolbar) findViewById(R.id.view_title);
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    /**
+     * 显示等待对话框
+     */
+    public void showProgressDialog() {
+        this.showProgressDialog("");
+    }
+
+    /**
+     * 取消等待对话框
+     */
+    public void dismissProgressDialog() {
+        try {
+            if (loadingDialog != null && !isFinishing()) {
+                loadingDialog.dismiss();
+                loadingDialog = null;
+                isLoading = false;
+            }
+        } catch (IllegalArgumentException e) {
+            Log.d("View", "loaded -> dialog already dismissed");
+        }
+    }
+
+    public void onBackPressed(View v) {
+        onBackPressed();
+    }
 
 }
