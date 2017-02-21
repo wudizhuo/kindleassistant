32b51e14ad4155936f7f78d71eb73488d777899b
wudizhuo <sunzhuo1228@126.com>
2017-02-20 20:55:35 -0600

    remove about activity

--- app/src/main/java/com/kindleassistant/fragment/SlidingMenuRight.java
+++ app/src/main/java/com/kindleassistant/fragment/SlidingMenuRight.java
@@ -12,7 +12,6 @@ import android.widget.LinearLayout.LayoutParams;
 import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
 import com.kindleassistant.App;
 import com.kindleassistant.R;
-import com.kindleassistant.activity.AboutActivity;
 import com.kindleassistant.activity.SettingActivity;
 import com.kindleassistant.activity.ShareExplainActivity;
 import com.kindleassistant.activity.UploadActivity;
@@ -43,7 +42,6 @@ public class SlidingMenuRight extends BaseFragment implements OnClickListener {
         checkUpdate = view.findViewById(R.id.check_update);
         checkUpdate.setOnClickListener(this);
         view.findViewById(R.id.share).setOnClickListener(this);
-        view.findViewById(R.id.about).setOnClickListener(this);
         view.findViewById(R.id.uploads).setOnClickListener(this);
         initBtn();
         return view;
@@ -70,9 +68,6 @@ public class SlidingMenuRight extends BaseFragment implements OnClickListener {
                 break;
             case R.id.share:
                 share();
-                break;
-            case R.id.about:
-                startActivity(new Intent(mContext, AboutActivity.class));
                 break;
             case R.id.uploads:
                 startActivity(new Intent(mContext, UploadActivity.class));
