70c831b80d6724f4bc86dee7b5e2af4f9fac7450
wudizhuo <sunzhuo1228@126.com>
2017-10-07 21:37:24 -0700

    refactor

--- app/src/main/java/com/kindleassistant/App.java
+++ app/src/main/java/com/kindleassistant/App.java
@@ -2,18 +2,12 @@ package com.kindleassistant;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 
 import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
 import com.kindleassistant.utils.ChannelUtil;
 import com.qihoo.updatesdk.lib.UpdateHelper;
-import com.tencent.android.tpush.XGIOperateCallback;
-import com.tencent.android.tpush.XGPushManager;
 import com.umeng.analytics.AnalyticsConfig;
-import com.umeng.analytics.MobclickAgent;
-import com.umeng.fb.FeedbackAgent;
 
 public class App extends Application {
     private static Context applicationContext;
@@ -24,16 +18,6 @@ public class App extends Application {
         super.onCreate();
         applicationContext = this.getApplicationContext();
         app = this;
-        checkConfig();
-    }
-
-    /**
-     * 检查应用配置
-     */
-    private void checkConfig() {
-        // 友盟的配置
-        // 调试模式
-        MobclickAgent.setDebugMode(BuildConfig.DEBUG);
     }
 
     public void appinit() {
@@ -41,23 +25,6 @@ public class App extends Application {
         FeedbackAPI.init(this, "23639140");
 
         initUpdateHelper();
-
-        Context context = getApplicationContext();
-        XGPushManager.registerPush(context, new XGIOperateCallback() {
-            @Override
-            public void onSuccess(Object data, int flag) {
-                Log.d("TPush", "注册成功，设备token为：" + data);
-                AppPreferences.setRegisterPush(true);
-            }
-
-            @Override
-            public void onFail(Object data, int errCode, String msg) {
-                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
-            }
-        });
-
-        FeedbackAgent agent = new FeedbackAgent(this);
-        agent.sync();
     }
 
     private void initUpdateHelper() {
@@ -76,18 +43,4 @@ public class App extends Application {
     public static App getApp() {
         return app;
     }
-
-    /**
-     * 获取app版本号。
-     */
-    public static String getVersionName() {
-        try {
-            return getContext().getPackageManager().getPackageInfo(
-                    getContext().getPackageName(), 0).versionName;
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 }
